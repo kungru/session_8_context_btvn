1cd9a1de67110925c37e62d5e8915840bcc15865
kungru <108171226+kungru@users.noreply.github.com>
2022-09-29 20:03:20 +0700

    add eng welcome

--- src/Page/Footer.js
+++ src/Page/Footer.js
@@ -1,27 +1,38 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import '../App.css';
 import ContextLanguage from '../context/ContextLanguage';
 const Footer = () => {
-    const lan=useContext(ContextLanguage);
-    const handleLanguage=(e)=>{
-        if(e.target.value=='vi'){
-            lan.setLanguage({
-                logo:'MindX',
-    login:'Đăng nhập',
-    t:'Đăng nhập',
-    placeholder:'Tên người dùng ...',
-    welcome:'Chào mừng bạn',
-    hello:'Xin chào'
-            })
-        } 
+  const lan = useContext(ContextLanguage);
+  const handleLanguage = (e) => {
+    if (e.target.value == 'vi') {
+      lan.setLanguage({
+        logo: 'MindX',
+        login: 'Đăng nhập',
+        t: 'Đăng nhập',
+        placeholder: 'Tên người dùng ...',
+        welcome: 'Chào mừng bạn',
+        hello: 'Xin chào'
+      })
+    } else {
+      lan.setLanguage(
+        {
+          logo: 'MindX',
+          login: 'Login',
+          t: 'Sign in',
+          placeholder: 'Username ...',
+          welcome: 'welcome',
+          hello: 'Welcome back',
+        }
+      )
     }
+  }
   return (
     <footer>
-        <select onChange={handleLanguage}>
-          <option>eng</option>
-          <option>vi</option>
-        </select>
-      </footer>
+      <select onChange={handleLanguage}>
+        <option>eng</option>
+        <option>vi</option>
+      </select>
+    </footer>
   )
 }
 
